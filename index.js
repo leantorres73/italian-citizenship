7064adeb15dee562e67a2ada9024d2bb9444f7f9
Leandro <leandrotorres73@gmail.com>
2022-06-02 17:40:26 -0300

    ciudadania

---
+++ index.js
@@ -0,0 +1,24 @@
+var superagent = require('superagent');
+var agent = superagent.agent();
+const cheerio = require('cheerio');
+
+const detectPassport = async () => {
+  await agent.post('https://prenotami.esteri.it/Home/Login');
+    .type('form')
+    .send({ Email: process.env.email, Password: process.env.password });
+  
+  const res = await agent.get('https://prenotami.esteri.it/Services/Booking/552');
+  //use cheerio to use jquery to select DOM elements
+  var $ = cheerio.load(res.text);
+
+  //select DOM elements using jquery selectors
+  $('#WlNotAvailable').filter(function(){
+      var data = $(this);
+      value = data.val();
+      if (value != 'Al momento non ci sono date disponibili per il servizio richiesto') {
+        // add telegram functionality
+      }
+  });
+}
+
+detectPassport();
