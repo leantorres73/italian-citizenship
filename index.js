8e1d7691b8f7197c13cd1b0a0a61edb1abcf06b7
Leandro <leandrotorres73@gmail.com>
2022-06-05 12:32:35 -0300

    login only once

--- index.js
+++ index.js
@@ -7,19 +7,27 @@ const minutes = process.env.MINUTES || 3;
 const interval = minutes * 60 * 1000;
 
 const detectPassport = async () => {
-  await agent.post('https://prenotami.esteri.it/Home/Login')
-    .type('form')
-    .send({ Email: process.env.email, Password: process.env.password });
-  
-  const res = await agent.get('https://prenotami.esteri.it/Services/Booking/552');
-  //use cheerio to use jquery to select DOM elements
+  let res = await query();
+  if (res.redirects[0] == 'https://prenotami.esteri.it/Home?ReturnUrl=%2fServices%2fBooking%2f552') {
+    await login();
+    res = await query();
+  }
   const root = parser.parse(res.text);
 
-  //select DOM elements using jquery selectors
   const selector = root.querySelector('#WlNotAvailable');
   if (!selector || !selector.attrs || selector.attrs.value != 'Al momento non ci sono date disponibili per il servizio richiesto') {
     await sendMessage('Ciudadania disponible');
   }
+}
+
+const query = async () => {
+  return await agent.get('https://prenotami.esteri.it/Services/Booking/552');  
+}
+
+const login = async () => {
+  await agent.post('https://prenotami.esteri.it/Home/Login')
+    .type('form')
+    .send({ Email: process.env.email, Password: process.env.password });
 }
 
 detectPassport();
