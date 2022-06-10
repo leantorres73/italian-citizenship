f479a6005ff6f5d2527855793d476681766ce903
Leandro <leandrotorres73@gmail.com>
2022-06-10 00:46:56 -0300

    ciudadania

--- index.js
+++ index.js
@@ -4,26 +4,27 @@ const agent = superagent.agent();
 const parser = require('node-html-parser');
 const { sendMessage } = require('./telegram');
 
+const path = '/Services/Booking/435';
 const minutes = process.env.MINUTES || 3; 
 const interval = minutes * 60 * 1000;
 
 const detectPassport = async () => {
   let res = await query();
-  if (res.redirects[0] == 'https://prenotami.esteri.it/Home?ReturnUrl=%2fServices%2fBooking%2f552') {
+  if (res.redirects[0] == 'https://prenotami.esteri.it/Home?ReturnUrl=%2fServices%2fBooking%2f435') {
     await login();
     res = await query();
   }
-  if (res.req.path == '/Services') {
+  if (res.req.path == path) {
     const root = parser.parse(res.text);
     const selector = root.querySelector('#WlNotAvailable');
     if (!selector || !selector.attrs || selector.attrs.value != 'Al momento non ci sono date disponibili per il servizio richiesto') {
-      await sendMessage('Pasaporte disponible');
+      await sendMessage('Ciudadania disponible');
     }
   }
 }
 
 const query = async () => {
-  return await agent.get('https://prenotami.esteri.it/Services/Booking/552');  
+  return await agent.get('https://prenotami.esteri.it' + path);  
 }
 
 const login = async () => {
