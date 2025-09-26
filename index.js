daf590b581601b751342bb6535518b76e5f9451c
leantorres73 <=>
2025-09-26 19:18:09 -0300

    change to passport

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const superagent = require('superagent');
-const fs = require('fs');
 const agent = superagent.agent();
 const parser = require('node-html-parser');
 const { sendMessage } = require('./telegram');
 
-const path = '/Services/Booking/435';
+const path = '/Services/Booking/4685';
 const minutes = process.env.MINUTES || 3; 
 const interval = minutes * 60 * 1000;
 
 const detectPassport = async () => {
   let res = await query();
-  if (res.redirects[0] == 'https://prenotami.esteri.it/Home?ReturnUrl=%2fServices%2fBooking%2f435') {
+  if (res.redirects[0] == 'https://prenotami.esteri.it/Home?ReturnUrl=%2fServices%2fBooking%2f4685') {
     await login();
     res = await query();
   }
   if (res.req.path == path) {
     const root = parser.parse(res.text);
     const selector = root.querySelector('#WlNotAvailable');
-    if (!selector || !selector.attrs || selector.attrs.value != 'Al momento non ci sono date disponibili per il servizio richiesto') {
-      await sendMessage('Ciudadania disponible');
+    if (!selector || !selector.attrs || selector.attrs.value != `Stante l'elevata richiesta i posti disponibili per il servizio scelto sono esauriti.`) {
+      await sendMessage('Pasaporte disponible');
     }
   }
 }
